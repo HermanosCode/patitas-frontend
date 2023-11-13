07df48fce5ee9f72ad6df02052a360bac1d9f9e1
FedericoHernanSanchez <federicohernansanchezof@gmail.com>
2023-11-13 20:34:16 -0300

    PopUp

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,22 @@ import { SideBarMenu } from './components/SideBar/SideBarMenu';
 import { SideBarMenuCard} from './types/SideBarMenu';
 import ProfileImage from './images/cv.jpg';
 import { SideBarItems } from "./utils/utils";
+import  { Fragment, useState } from 'react';
+import  PopUp  from "./components/PopUp/PopUp";
+import LoginSingUp from "./components/Login-SingUp/LoginSingUp"
 
 function App() {
   
-  /*Esta data vendria del backend */
+    return (
+      <>
+      <PopUp status={500} message='Su mascota ha sido publicada correctamente'/>
+      <LoginSingUp/>
+      </>
+      
+      
+    )
+  /*
+  /*Esta data vendria del backend 
   const card : SideBarMenuCard= {
     id : "1" ,
     displayName: "Juan Cruz Sanchez" ,
@@ -17,6 +29,7 @@ function App() {
   return (
    <SideBarMenu items={SideBarItems} card={card} />
   )
+  */
 }
 
 export default App
